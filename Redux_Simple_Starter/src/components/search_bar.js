0b547cf54dc7cb999c61aa6b57bd52fb0ad7d24a
KyoungRan <kyoungrankang@gmail.com>
2016-06-27 02:07:16 -0300

    Section1: An Intro to React(done)

--- Redux_Simple_Starter/src/components/search_bar.js
+++ Redux_Simple_Starter/src/components/search_bar.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ''};
+  }
+
   render() {
     return (
       <div>
-        <input onChange={this.onInputChange} />
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
       </div>
     );
-  }
-
-  onInputChange(event) {
-    { /* console.log(event.target.value); */ }
-    console.log(event);
   }
 }
 
