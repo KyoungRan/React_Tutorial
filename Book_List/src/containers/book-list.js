45cfbab445c6a62568f7efbb256bfcb155c2c76d
KyoungRan <kyoungrankang@gmail.com>
2016-07-04 17:23:33 -0300

    S4: Managing App State with Redux

--- Book_List/src/containers/book-list.js
+++ Book_List/src/containers/book-list.js
@@ -9,7 +9,7 @@ class BookList extends Component {
       return (
         <li
           key={book.title}
-          onClick={() => this.props.selectBook()}
+          onClick={() => this.props.selectBook(book)}
           className="list-group-item">
           {book.title}
         </li>
@@ -37,11 +37,12 @@ function mapStateToProps(state) {
 // Anything returned from this function will end up as props
 // on the BookList container
 function mapDispatchToProps(dispatch) {
-  // Whenever selectBook is called, the result should be passed
+  // Whenever selectBook is called, the result shoudl be passed
   // to all of our reducers
   return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
 // Promote BookList from a component to a container - it needs to know
-// about this new dispatch method, selectBook. Make it available as prop.
+// about this new dispatch method, selectBook. Make it available
+// as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- Book_List/src/reducers/reducer_active_book.js
+++ Book_List/src/reducers/reducer_active_book.js
@@ -1,9 +1,9 @@
-// State argumenet is not application state, only the state
+// State argument is not application state, only the state
 // this reducer is responsible for
 export default function(state = null, action) {
   switch(action.type) {
-    case 'BOOK_SELECTED':
-      return action.payload;
+  case 'BOOK_SELECTED':
+    return action.payload;
   }
 
   return state;
