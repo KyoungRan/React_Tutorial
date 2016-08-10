c7c8954fd1b1f0e6c80f3782631d36298a48acdd
KyoungRan <kyoungrankang@gmail.com>
2016-08-10 17:11:56 +0900

    Redux #8 - Integrating our Store with React Router

--- ReduxStagram/client/store.js
+++ ReduxStagram/client/store.js
@@ -1,4 +1,4 @@
-import { createstore, compse } from 'redux';
+import { createStore, compse } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
