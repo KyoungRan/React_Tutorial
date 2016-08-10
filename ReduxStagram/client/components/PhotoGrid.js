a6e0c2b9801b99c0a18dfa7d1959d70fd6fa338c
KyoungRan <kyoungrankang@gmail.com>
2016-08-10 20:55:41 +0900

    Redux #11 - Displaying Redux State inside our Components

--- ReduxStagram/client/components/PhotoGrid.js
+++ ReduxStagram/client/components/PhotoGrid.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass ({
   render() {
     return (
-      <div>I'm the photo grid</div>
+      <div className="photo-grid">
+      {/*
+        <pre>
+          {JSON.stringify(this.props.posts, null, ' ')}
+        </pre>
+      */}
+      {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+      </div>
     )
   }
 });
