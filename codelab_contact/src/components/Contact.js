a91e57f82810f8c0be9a1b744404cab5547e83c9
KyoungRan <kyoungrankang@gmail.com>
2016-12-05 19:34:33 +0900

    4-3: immutability helper /es6 spread

--- codelab_contact/src/components/Contact.js
+++ codelab_contact/src/components/Contact.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends Component {
   constructor(props) {
