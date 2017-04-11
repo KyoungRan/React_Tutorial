b3b9f231bb59fb9b4055546eaf80d7e0458f211b
KyoungRan <kyoungrankang@gmail.com>
2017-04-12 00:30:54 +0900

    just saved

---
+++ googleMpas_ReactComponent/webpack.config.js
@@ -0,0 +1,35 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+require("babel-polyfill");
+var Promise = require("bluebird");
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+};
